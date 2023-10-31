9327a6b393b3dec9711ebd29fc527c78e6bcbcc2
Mayra Regina Nascimento de Oliveira <mayra.regina@academico.ifrn.edu.br>
2023-10-31 14:24:10 -0300

    31.10

--- Mayra/Assets/Script/Enemy.cs
+++ Mayra/Assets/Script/Enemy.cs
@@ -53,7 +53,7 @@ public class Enemy : MonoBehaviour
                 if (!atacking)
                 {
                     agent.SetDestination(player.position);
-                    anim.SetBool("Slither Forward", true);
+                    anim.SetBool("Run Forward", true);
                     walking = true;
                 }
 
@@ -68,7 +68,7 @@ public class Enemy : MonoBehaviour
             }
             else
             {
-                anim.SetBool("Slither Forward", false);
+                anim.SetBool("Run Forward", false);
                 atacking = false;
                 walking = false;
                 movetowaypoint();
@@ -83,7 +83,7 @@ public class Enemy : MonoBehaviour
             waitfor = true;
             atacking = true;
             walking = false;
-            anim.SetBool("Slither Forward", false);
+            anim.SetBool("Run Forward", false);
             anim.SetBool("Bite Attack", true);
             yield return new WaitForSeconds(1.2f);
             GetPlayer();
@@ -92,7 +92,7 @@ public class Enemy : MonoBehaviour
 
         if (playerdead)
         {
-            anim.SetBool("Slither Forward", false);
+            anim.SetBool("Run Forward", false);
             anim.SetBool("Bite Attack", false);
             walking = false;
             atacking = false;
@@ -131,7 +131,7 @@ public class Enemy : MonoBehaviour
     IEnumerator recovery()
     {
         yield return new WaitForSeconds(1f);
-        anim.SetBool("Slither Forward", false);
+        anim.SetBool("Run Forward", false);
         anim.SetBool("Bite Attack", false);
         hitting = false;
         waitfor = false;
@@ -149,7 +149,7 @@ public class Enemy : MonoBehaviour
                 currentpatch = Random.Range(0, points.Count);
                 
             }
-            anim.SetBool("Slither Forward", true);
+            anim.SetBool("Run Forward", true);
             walking = true;
         }
     }
